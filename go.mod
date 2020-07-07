aa4f75ef4acd702d3f93982c29191300efaac942
Bryan Tomlin <bdtomlin@gmail.com>
2020-07-06 19:36:27 -0500

    conform to fiber/template api

--- go.mod
+++ go.mod
@@ -2,6 +2,11 @@ module github.com/bdtomlin/tplus
 
 go 1.14
 
-replace github.com/gofiber/template => /Users/bryantomlin/dev/template-bt
-
-require github.com/gofiber/template v1.4.1
+require (
+	github.com/gofiber/template v1.5.3
+	github.com/juju/errors v0.0.0-20181118221551-089d3ea4e4d5 // indirect
+	github.com/juju/loggo v0.0.0-20180524022052-584905176618 // indirect
+	github.com/juju/testing v0.0.0-20180920084828-472a3e8b2073 // indirect
+	gopkg.in/mgo.v2 v2.0.0-20180705113604-9856a29383ce // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
+)
--- tplus.go
+++ tplus.go
@@ -1,15 +1,16 @@
 package tplus
 
 import (
-	"errors"
 	"fmt"
 	"html/template"
 	"io"
-	"io/ioutil"
-	"log"
+	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
+
+	"github.com/gofiber/template/utils"
 )
 
 const (
@@ -20,33 +21,100 @@ const (
 
 // Engine struct
 type Engine struct {
+	// delimiters
+	left  string
+	right string
+	// views folder
 	directory string
+	// http.FileSystem supports embedded files
+	fileSystem http.FileSystem
+	// views extension
 	extension string
-
+	// layout variable name that incapsulates the template
+	layout string
+	// reload on each render
+	reload bool
+	// debug prints the parsed templates
+	debug bool
+	// lock for funcmap and templates
+	mutex sync.RWMutex
+	// template funcmap
+	funcmap map[string]interface{}
+	// templates
 	Templates *template.Template
 }
 
 // New returns a HTML render engine for Fiber
-func New(directory, extension string, funcmap ...map[string]interface{}) *Engine {
+func New(directory, extension string) *Engine {
 	engine := &Engine{
+		left:      "{{",
+		right:     "}}",
 		directory: directory,
 		extension: extension,
-		Templates: template.New(directory),
-	}
-	if len(funcmap) > 0 {
-		engine.Templates.Funcs(funcmap[0])
-	}
-
-	if err := engine.Parse(); err != nil {
-		log.Fatalf("html.New(): %v", err)
+		layout:    tplusToken,
+		funcmap:   make(map[string]interface{}),
 	}
 	return engine
 }
 
-// Parse parses the templates to the engine.
-func (e *Engine) Parse() error {
+func NewFileSystem(fs http.FileSystem, extension string) *Engine {
+	engine := &Engine{
+		left:       "{{",
+		right:      "}}",
+		directory:  "/",
+		fileSystem: fs,
+		extension:  extension,
+		layout:     tplusToken,
+		funcmap:    make(map[string]interface{}),
+	}
+	return engine
+}
+
+// Delims sets the action delimiters to the specified strings, to be used in
+// templates. An empty delimiter stands for the
+// corresponding default: {{ or }}.
+func (e *Engine) Delims(left, right string) *Engine {
+	e.left, e.right = left, right
+	return e
+}
+
+// AddFunc adds the function to the template's function map.
+// It is legal to overwrite elements of the default actions
+func (e *Engine) AddFunc(name string, fn interface{}) *Engine {
+	e.mutex.Lock()
+	e.funcmap[name] = fn
+	e.mutex.Unlock()
+	return e
+}
+
+// Reload if set to true the templates are reloading on each render,
+// use it when you're in development and you don't want to restart
+// the application when you edit a template file.
+func (e *Engine) Reload(enabled bool) *Engine {
+	e.reload = enabled
+	return e
+}
+
+// Debug will print the parsed templates when Load is triggered.
+func (e *Engine) Debug(enabled bool) *Engine {
+	e.debug = enabled
+	return e
+}
+
+// Load the templates to the engine.
+func (e *Engine) Load() error {
+	// race safe
+	e.mutex.Lock()
+	defer e.mutex.Unlock()
+
+	e.Templates = template.New(e.directory)
+
+	// Set template settings
+	e.Templates.Delims(e.left, e.right)
+	e.Templates.Funcs(e.funcmap)
+
 	// Loop trough each directory and register template files
-	err := filepath.Walk(e.directory, func(path string, info os.FileInfo, err error) error {
+	walkFn := func(path string, info os.FileInfo, err error) error {
 		// Return error if exist
 		if err != nil {
 			return err
@@ -71,36 +139,48 @@ func (e *Engine) Parse() error {
 		// partials\footer.tmpl -> partials/footer.tmpl
 		name := filepath.ToSlash(rel)
 		// Remove ext from name 'index.tmpl' -> 'index'
-		name = strings.Replace(name, e.extension, "", -1)
+		name = strings.TrimSuffix(name, e.extension)
 		// Read the file
 		// #gosec G304
-		buf, err := ioutil.ReadFile(path)
+		buf, err := utils.ReadFile(path, e.fileSystem)
 		if err != nil {
 			return err
 		}
-		// Create new template associated with the current one
-		// This enable use to invoke other templates {{ template .. }}
-		// _, err = e.Templates.New(name).Parse(string(buf))
+
+		// err = e.ParseTplusComponents(name, string(buf))
+		// if err != nil {
+		// 	return err
+		// }
 
 		err = e.ParseTplusTokens(name, string(buf))
 		if err != nil {
 			return err
 		}
 		// Debugging
+		// if e.debug {
+		// 	fmt.Printf("views: parsed template: %s\n", name)
+		// }
 		//fmt.Printf("[Engine] Registered view: %s\n", name)
 		return err
-	})
-	return err
+	}
+
+	if e.fileSystem != nil {
+		return utils.Walk(e.fileSystem, e.directory, walkFn)
+	}
+	return filepath.Walk(e.directory, walkFn)
 }
 
 // ParseTplusTokens takes a template string and breaks it up into sub templates
 // to be used for wrapping the tplusContent tokens
 func (e *Engine) ParseTplusTokens(name string, content string) error {
 	// fmt.Println(content)
-	tpls := strings.Split(content, tplusToken)
+	tpls := strings.Split(content, e.layout)
 	switch len(tpls) {
 	case 1:
 		_, err := e.Templates.New(name).Parse(tpls[0])
+		if e.debug {
+			fmt.Printf("views: parsed template: %s\n", name)
+		}
 		return err
 	case 2:
 		_, err := e.Templates.New(tplusHead + name).Parse(tpls[0])
@@ -111,6 +191,10 @@ func (e *Engine) ParseTplusTokens(name string, content string) error {
 		if err != nil {
 			return err
 		}
+		if e.debug {
+			fmt.Printf("views: parsed template: %s\n", tplusHead+name)
+			fmt.Printf("views: parsed template: %s\n", tplusTail+name)
+		}
 		return nil
 	case 3:
 		return fmt.Errorf("too many tplus tokens in %v", name)
@@ -119,16 +203,19 @@ func (e *Engine) ParseTplusTokens(name string, content string) error {
 }
 
 // Render will render the template by name as well as wrap any layouts around it in left to right order
-func (e *Engine) Render(out io.Writer, name string, binding interface{}, layouts ...string) error {
-	if len(layouts) == 0 {
-		return e.Templates.ExecuteTemplate(out, name, binding)
+func (e *Engine) Render(out io.Writer, template string, binding interface{}, layouts ...string) error {
+	if e.reload {
+		if err := e.Load(); err != nil {
+			return err
+		}
 	}
+
 	for i := len(layouts) - 1; i >= 0; i-- {
 		if err := e.Templates.ExecuteTemplate(out, tplusHead+layouts[i], binding); err != nil {
 			return err
 		}
 	}
-	if err := e.Templates.ExecuteTemplate(out, name, binding); err != nil {
+	if err := e.Templates.ExecuteTemplate(out, template, binding); err != nil {
 		return err
 	}
 	for _, l := range layouts {
@@ -136,6 +223,5 @@ func (e *Engine) Render(out io.Writer, name string, binding interface{}, layouts
 			return err
 		}
 	}
-	// more performant than fmt.Errorf
-	return errors.New(strings.Join([]string{"error executing template "}, name))
+	return nil
 }
